5fa8f545aec68fd2143f1be7409861e6e11c5e6e
Arvind Gavas <arvindgawas@yahoo.com>
2020-11-12 12:59:32 +0530

    first

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bitify.Data;
+using bitify.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace bitify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UserController : ControllerBase
+    {
+
+
+        private DataContext _dbcontext;
+        public UserController(DataContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>  GetUsers()
+        {
+            return await _dbcontext.Users.ToListAsync();
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dbcontext.Users.FindAsync(id);
+        }
+
+        
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bitify.Data;
+using bitify.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace bitify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly DataContext _dbcontext;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext dbcontext)
+        {
+            _logger = logger;
+            _dbcontext = dbcontext;
+        }
+
+        /*
+        [HttpGet]
+        public IEnumerable<AppUser> GetUsers()
+        {
+            return _dbcontext.Users.ToList();
+        }
+        */
+        
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
